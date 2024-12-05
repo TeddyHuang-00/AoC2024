3f3db6c05dbb75aa349e3f305cd2980c8fce4788
Nan Huang <teddyhuangnan@gmail.com>
2024-12-05 04:38:38 -0500

    feat: :sparkles: Add seq-macro for dynamic module and enum generation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
 mod solution;
 
 use clap::{Parser, ValueEnum};
+use seq_macro::seq;
 use solution::Solution;
 use std::{fs, process::exit};
 
@@ -14,14 +10,14 @@ pub const RIN: &str = "examples/inputs";
 /// Example output file root path
 pub const ROUT: &str = "examples/outputs";
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
-pub enum Day {
-    Day1,
-    Day2,
-    Day3,
-    Day4,
-    Day5,
-}
+seq!(N in 1..=5 {
+    #(mod day~N;)*
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+    pub enum Day {
+        #(Day~N,)*
+    }
+});
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -38,13 +34,12 @@ struct Args {
 fn main() {
     let args: Args = Args::parse();
     let day_num = args.day as u8 + 1;
-    let solution: Box<dyn Solution> = match args.day {
-        Day::Day1 => Box::new(day1::Puzzle),
-        Day::Day2 => Box::new(day2::Puzzle),
-        Day::Day3 => Box::new(day3::Puzzle),
-        Day::Day4 => Box::new(day4::Puzzle),
-        Day::Day5 => Box::new(day5::Puzzle),
-    };
+
+    seq!(N in 1..=5 {
+        let solution: Box<dyn Solution> = match args.day {
+            #(Day::Day~N => Box::new(day~N::Puzzle),)*
+        };
+    });
     let input =
         fs::read_to_string(format!("{}/day{}.txt", args.input, day_num)).unwrap_or_else(|_| {
             println!("Day {day_num}: No input file found");
