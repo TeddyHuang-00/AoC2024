a4df468541177a550849a38d1c120f30b00ace63
Nan Huang <teddyhuangnan@gmail.com>
2024-12-18 11:37:19 -0600

    perf: :zap: Use integer solution to avoid float to integer conversion

--- src/day13.rs
+++ src/day13.rs
@@ -1,12 +1,30 @@
-use nalgebra::{Matrix2, Vector2};
+use std::ops::Add;
+
 use regex::Regex;
 
 use crate::solution::Solution;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Coord {
+    x: i64,
+    y: i64,
+}
+
+impl Add for Coord {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
 pub struct Puzzle;
 
 impl Puzzle {
-    fn parse_input(input: &str) -> Vec<(Matrix2<f64>, Vector2<f64>)> {
+    fn parse_input(input: &str) -> Vec<(Coord, Coord, Coord)> {
         let x_pattern = Regex::new(r"X(?:\+|=)(\d+)").unwrap();
         let y_pattern = Regex::new(r"Y(?:\+|=)(\d+)").unwrap();
         let get = |s: &str, pat: &Regex| {
@@ -15,7 +33,7 @@ impl Puzzle {
                 .get(1)
                 .unwrap()
                 .as_str()
-                .parse::<f64>()
+                .parse()
                 .unwrap()
         };
         input
@@ -23,16 +41,47 @@ impl Puzzle {
             .map(|c| {
                 let mut c = c.lines();
                 let (a, b, g) = (c.next().unwrap(), c.next().unwrap(), c.next().unwrap());
-                let basis = Matrix2::new(
-                    get(a, &x_pattern),
-                    get(b, &x_pattern),
-                    get(a, &y_pattern),
-                    get(b, &y_pattern),
-                );
-                let goal = Vector2::new(get(g, &x_pattern), get(g, &y_pattern));
-                (basis, goal)
+                let a = Coord {
+                    x: get(a, &x_pattern),
+                    y: get(a, &y_pattern),
+                };
+                let b = Coord {
+                    x: get(b, &x_pattern),
+                    y: get(b, &y_pattern),
+                };
+                let g = Coord {
+                    x: get(g, &x_pattern),
+                    y: get(g, &y_pattern),
+                };
+                (a, b, g)
             })
             .collect()
+    }
+
+    fn linear_solution(a: Coord, b: Coord, g: Coord) -> Option<i64> {
+        let _ = "
+        | a.x b.x | | x |   | g.x |
+        | a.y b.y | | y | = | g.y |
+
+        | x |   | a.x b.x |^-1 | g.x |
+        | y | = | a.y b.y |    | g.y |
+
+        | x |   | b.y -b.x | | g.x |  /
+        | y | = |-a.y  a.x | | g.y | / det
+        ";
+        let det = a.x * b.y - a.y * b.x;
+        if det == 0 {
+            return None;
+        }
+        let s = Coord {
+            x: b.y * g.x - b.x * g.y,
+            y: a.x * g.y - a.y * g.x,
+        };
+        // Check if the solution is an integer
+        if s.x % det != 0 || s.y % det != 0 {
+            return None;
+        }
+        Some(s.x / det * 3 + s.y / det)
     }
 }
 
@@ -40,37 +89,20 @@ impl Solution for Puzzle {
     fn part1(&self, input: &str) -> String {
         Self::parse_input(input)
             .into_iter()
-            .filter_map(|(basis, goal)| {
-                let cnt = basis.try_inverse().unwrap() * goal;
-                let x = cnt.x.round();
-                let y = cnt.y.round();
-                // Determine if the solution is an integer, be caution with floating point errors
-                if (cnt.x - x).abs().fract() < 1e-3 && (cnt.y - y).abs().fract() < 1e-3 {
-                    Some(x as u64 * 3 + y as u64)
-                } else {
-                    None
-                }
-            })
-            .sum::<u64>()
+            .filter_map(|(a, b, g)| Self::linear_solution(a, b, g))
+            .sum::<i64>()
             .to_string()
     }
 
     fn part2(&self, input: &str) -> String {
+        let offset = Coord {
+            x: 1e13 as i64,
+            y: 1e13 as i64,
+        };
         Self::parse_input(input)
             .into_iter()
-            .filter_map(|(basis, goal)| {
-                let goal = goal.add_scalar(1e13);
-                let cnt = basis.try_inverse().unwrap() * goal;
-                let x = cnt.x.round();
-                let y = cnt.y.round();
-                // Determine if the solution is an integer, be caution with floating point errors
-                if (cnt.x - x).abs().fract() < 1e-3 && (cnt.y - y).abs().fract() < 1e-3 {
-                    Some(x as u64 * 3 + y as u64)
-                } else {
-                    None
-                }
-            })
-            .sum::<u64>()
+            .filter_map(|(a, b, g)| Self::linear_solution(a, b, g + offset))
+            .sum::<i64>()
             .to_string()
     }
 }
